7f88d97b754c34d0b2e19a448f63b278fd72c555
benmcgarvey <ben@sidekicker.com.au>
2021-09-27 10:08:22 +1000

    render table

--- src/components/Tables.tsx
+++ src/components/Tables.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { Cell, Column, Table } from '@blueprintjs/table';
+import '@blueprintjs/table/lib/css/table.css';
 import useDB from './DBContext';
 
 const Tables = () => {
   const { queryResult, queryError } = useDB();
 
+  const { values, columns } = useMemo(
+    () => ({
+      values: queryResult?.[0]?.values ?? [],
+      columns: queryResult?.[0]?.columns ?? [],
+    }),
+    [queryResult]
+  );
+
   return (
-    <>
-      <div>{JSON.stringify(queryResult)}</div>
+    <div>
+      <Table numRows={values.length}>
+        {columns.map((column) => (
+          <Column
+            key={column}
+            name={column}
+            cellRenderer={(rowIndex: number, columnIndex: number) => (
+              <Cell>{values[rowIndex]?.[columnIndex]}</Cell>
+            )}
+          />
+        ))}
+      </Table>
       <div>{JSON.stringify(queryError)}</div>
-    </>
+    </div>
   );
 };
 
