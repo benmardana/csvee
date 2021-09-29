4a26103b0c3d7bbf496b4d211f08891c53573003
benmcgarvey <ben@sidekicker.com.au>
2021-09-29 16:29:13 +1000

    adds monaco editor

--- src/components/SQLEditor.tsx
+++ src/components/SQLEditor.tsx
@@ -1,30 +1,45 @@
-import { Button, Intent, TextArea } from '@blueprintjs/core';
+import { Button } from '@blueprintjs/core';
 import React, { useState } from 'react';
+import Editor from '@monaco-editor/react';
 
 import useDB from './DBContext';
 
 const SQLEditor = () => {
   const { executeQuery } = useDB();
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState<string>();
 
-  const handleExecuteQuery = () => executeQuery?.(query);
+  const handleExecuteQuery = () => query && executeQuery?.(query);
   return (
-    <div style={{ position: 'relative', minWidth: '100%', height: '30%' }}>
-      <TextArea
-        style={{ minWidth: '100%', height: '100%' }}
-        intent={Intent.PRIMARY}
-        onChange={(event) => setQuery(event.target.value)}
+    <div
+      style={{
+        minWidth: '100%',
+        height: '30%',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Editor
+        height="100%"
+        defaultLanguage="sql"
         value={query}
-        className="code"
-        large
+        onChange={setQuery}
+        options={{ minimap: { enabled: false } }}
       />
-      <Button
-        onClick={handleExecuteQuery}
-        style={{ position: 'absolute', bottom: 0, right: 0 }}
-        outlined
-      >
-        Execute
-      </Button>
+      <div style={{ boxShadow: '0 0 0 1px rgb(16 22 26 / 15%)', zIndex: 0 }}>
+        <Button
+          onClick={handleExecuteQuery}
+          style={{
+            paddingLeft: 16,
+            paddingRight: 16,
+            margin: 16,
+            float: 'right',
+          }}
+          outlined
+          small
+        >
+          Execute
+        </Button>
+      </div>
     </div>
   );
 };
